4d1d98d2bee2d201e640cf968b7170c22d5e7735
Lary <larissa.mendy@gmail.com>
2026-03-17 09:50:26 -0300

    Alteração

--- src/auth/constants/constants.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConstants = {
-    secret: '2cefef0ab037bc1195a06645f5ec56e11dc5957c8c82b1287cc442f7245de71b',
-};
--- src/auth/entities/usuariologin.entity.ts
+++
@@ -1,7 +0,0 @@
-export class UsuarioLogin {
-
-    public usuario: string
-
-    public senha: string
-
-}

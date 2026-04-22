d8c6625e70341cc0869c77a5a29c66aa97fd2d78
Lary <larissa.mendy@gmail.com>
2026-04-22 10:59:57 -0300

    💥 NEW: Adicionando o CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+  origin: '*', // ou especifique: 'http://localhost:5173'
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   const config = new DocumentBuilder()
   .setTitle('ConnectElas')
   .setDescription('Projeto ConnectElas')
